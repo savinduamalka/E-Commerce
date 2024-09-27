de89faf4ab524c9e00fef7ed1b4a83e8afff89b4
Savindu Amalka <savinduamalka@gmail.com>
2024-09-27 23:00:24 +0530

    Created site structure

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,22 +1,38 @@
-import React, { useState } from 'react'
-import './Navbar.css'
-import logo from '../Assets/logo.png'
-import cart_icon from '../Assets/cart_icon.png'
-import { Link } from 'react-router-dom'
+import React, { useState, useEffect } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import './Navbar.css';
+import logo from '../Assets/logo.png';
+import cart_icon from '../Assets/cart_icon.png';
 
 const Navbar = () => {
-    const [menu,setMenu]=useState("shop");
+  const location = useLocation();
+  const [menu, setMenu] = useState("shop");
+
+  useEffect(() => {
+    if (location.pathname === "/") {
+      setMenu("shop");
+    } else if (location.pathname === "/mens") {
+      setMenu("mens");
+    } else if (location.pathname === "/womens") {
+      setMenu("womens");
+    } else if (location.pathname === "/kids") {
+      setMenu("kids");
+    }else{
+      setMenu(null);
+    }
+  }, [location.pathname]);
+
   return (
     <div className='navbar'>
-        <div className="nav-logo">
-            <Link to='/'><img src={logo} alt="" /></Link>
-            <p>SHOPPER</p>
-        </div>
+      <div className="nav-logo">
+        <Link to='/'><img src={logo} alt="" /></Link>
+        <p>SHOPPER</p>
+      </div>
       <ul className="nav-menu">
-        <li onClick={()=>{setMenu("shop")}}><Link style={{textDecoration: 'none' }}to='/'>Shop</Link>{menu==="shop"?<hr/>:<></>}</li>
-        <li onClick={()=>{setMenu("mens")}}><Link style={{textDecoration: 'none' }}to='/mens'>Men</Link>{menu==="mens"?<hr/>:<></>}</li>
-        <li onClick={()=>{setMenu("womens")}}><Link style={{textDecoration: 'none' }}to='/womens'>Women</Link>{menu==="womens"?<hr/>:<></>}</li>
-        <li onClick={()=>{setMenu("kids")}}><Link style={{textDecoration: 'none' }}to='/kids'>Kids</Link>{menu==="kids"?<hr/>:<></>}</li>
+        <li onClick={() => setMenu("shop")}><Link style={{ textDecoration: 'none' }} to='/'>Shop</Link>{menu === "shop" ? <hr /> : null}</li>
+        <li onClick={() => setMenu("mens")}><Link style={{ textDecoration: 'none' }} to='/mens'>Men</Link>{menu === "mens" ? <hr /> : null}</li>
+        <li onClick={() => setMenu("womens")}><Link style={{ textDecoration: 'none' }} to='/womens'>Women</Link>{menu === "womens" ? <hr /> : null}</li>
+        <li onClick={() => setMenu("kids")}><Link style={{ textDecoration: 'none' }} to='/kids'>Kids</Link>{menu === "kids" ? <hr /> : null}</li>
       </ul>
       <div className="nav-login-cart">
         <Link to='/login'><button>Login</button></Link>
@@ -24,7 +40,7 @@ const Navbar = () => {
         <div className="nav-cart-count">0</div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ShopContext } from '../Contex/ShopContext'
+import { useParams } from 'react-router-dom';
+import Breadcrum from '../Components/Breadcrums/Breadcrum';
+import ProductDisplay from '../Components/ProductDisplay/ProductDisplay';
 
 const Product = () => {
+  const {all_products}=useContext(ShopContext);
+  const {productId} = useParams();
+  const product = all_products.find((e)=> e.id===Number(productId))
   return (
     <div>
-      
+      <Breadcrum product={product}/>
+      <ProductDisplay product={product}/>
     </div>
   )
 }
